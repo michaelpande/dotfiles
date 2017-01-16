8c74a4433c5644d6e3bffc485873078f6f525f3c
Michael Pande <michaelpande@gmail.com>
2017-01-16 20:00:56 +0100

    Gitignore, shellcommands, aliases etc

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 # Remove and symlink dotfile
 rm -f ~/.vimrc
-rm -f ~/.oh-my-zsh/custom/alias.zsh
+rm -f ~/.oh-my-zsh/custom/shell.zsh
 rm -f ~/.Xmodmap
+rm -f ~/.gitignore
 
+# Symlin
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/alias.zsh ~/.oh-my-zsh/custom/alias.zsh
 ln -s ~/dotfiles/.Xmodmap ~/.Xmodmap
 
 
@@ -16,3 +17,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -P ~/.vim/colors/
 
 vim +PluginInstall +qall
+
+# Set up shell
+cat ~/dotfiles/shell/* > ~/.oh-my-zsh/custom/shell.zsh
+
+# Set up gitignore
+cat ~/dotfiles/git/gitignore/* > ~/.gitignore
+
---
+++ shell/README.md
@@ -0,0 +1,1 @@
+# All files in this folder will be concatenated into one file and run on boot
