bf0da72593b06979acb966220f17de901efe12b3
Michael Pande <michaelpande@gmail.com>
2017-01-21 19:03:59 +0100

    -

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ rm -rf ~/.antigen
 rm -rf ~/.local/share/radiotray/bookmarks.xml
 rm -rf ~/.workrave/state
 rm -rf ~/.config/redshift.conf
-
+rm -rf ~/.xbindkeysrc
 # Make necessary folders
 mkdir ~/.i3
 mkdir ~/.antigen
@@ -20,15 +20,15 @@ curl https://cdn.rawgit.com/zsh-users/antigen/v1.3.4/bin/antigen.zsh > ~/.antige
 
 
 # Symlink
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.Xmodmap ~/.Xmodmap
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/Xmodmap ~/.Xmodmap
 ln -s ~/dotfiles/desktop/.i3/config ~/.i3/config
 ln -s ~/dotfiles/desktop/.i3/i3status.conf ~/.i3status.conf
 ln -s ~/dotfiles/etc/bookmarks.xml ~/.local/share/radiotray/bookmarks.xml
 ln -s ~/dotfiles/etc/workrave/state ~/.workrave/state
 ln -s ~/dotfiles/etc/redshift.conf ~/.config/redshift.conf 
-
+ln -s ~/dotfiles/xbindkeysrc ~/.xbindkeysrc
 
 # Vim setup
 rm -rf ~/.vim
